97dea1d4c03adcbbf771d5b9143cecbdb5db5b54
Eric Davies <iamed2@gmail.com>
2015-08-19 16:47:26 -0500

    Fixed that monstrous test error (it was @doc)

--- test/test_arbiter.jl
+++ test/test_arbiter.jl
@@ -1,8 +1,6 @@
 # Tests for the ArbiterTask type.
 
-"""
-Ensure that Arbiter is importable from the root module.
-"""
 facts("imports") do
+	# Ensure that Arbiter is importable from the root module.
 	@fact isa(ArbiterTask, DataType) --> true
 end
